ce396d8e439a64f19505575958a97a90b253a006
Hridyansh Sharma <hshar085@uottawa.ca>
2021-02-06 22:59:43 -0500

    detect posts with a NYSE ticker
    next step: write comments of stock posts to file for sentiment analysis

--- reddit_wsb/search_wsb.py
+++ reddit_wsb/search_wsb.py
@@ -11,7 +11,11 @@
 
 # for submission in submissions:
 #     print(submission.title)
+from io import StringIO
 import praw
+import csv
+from csv import reader
+from praw.models import MoreComments
 import pprint
 reddit = praw.Reddit(
     client_id="lgFgtNzEtdL6UQ",
@@ -21,19 +25,34 @@ reddit = praw.Reddit(
     password="uottahack2021"
 )
 
-print(reddit.read_only)
+subreddit = reddit.subreddit("stocks")
 
-# assume you have a reddit instance bound to variable `reddit`
-subreddit = reddit.subreddit("wallstreetbets")
-
-print(subreddit.display_name)  # output: redditdev
+with open("stocklist.txt") as f:
+    stockList = f.readlines()
+    stockList = [x.strip('\n') for x in stockList] 
+# you may also want to remove whitespace characters like `\n` at the end of each line
+#print(stockList)
 
 # assume you have a Subreddit instance bound to variable `subreddit`
-for submission in subreddit.hot(limit=1):
-    print(submission.title.encode('cp1252', errors='replace').decode('cp1252'))  # Output: the submission's title
-    print(submission.url)    # Output: the URL the submission points to
-    print()    
-
-for top_level_comment in submission.comments:
-    print(top_level_comment.body.encode('cp1252', errors='replace').decode('cp1252'))
-    print()
+substring = "GME"
+for submission in subreddit.new():#limit=100):
+    currPost = submission.title.encode('cp1252', errors='replace').decode('cp1252') 
+    #print(currPost)
+    for stock in stockList:
+        if str(stock) in currPost:
+            print("STOCK: " + stock)
+            print("TITLE: " + currPost)
+    #if any(stock in currPost for stock in stockList):
+        # print(stock)
+        # print("TITLE: " + currPost)
+    #if any(str(stock) in li for stock in stock_list):
+    #print("Found: " + stock)
+       # print(li)
+    #print("POST TITLE: " + submission.title.encode('cp1252', errors='replace').decode('cp1252'))  # Output: the submission's title
+    # print(submission.url)    # Output: the URL the submission points to
+    #print()   
+    # for top_level_comment in submission.comments:
+    #     if isinstance(top_level_comment, MoreComments):
+    #         continue
+    #     print(top_level_comment.body.encode('cp1252', errors='replace').decode('cp1252'))
+    # #print()
