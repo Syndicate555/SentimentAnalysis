50022030b5c2a5da3845753ec3e3163229fbeb50
Hridyansh Sharma <hshar085@uottawa.ca>
2021-02-07 02:02:53 -0500

    able to filter comments based on TICKER

--- reddit_wsb/search_wsb.py
+++ reddit_wsb/search_wsb.py
@@ -1,16 +1,3 @@
-# from psaw import PushshiftAPI
-# api = PushshiftAPI()
-
-# import datetime as dt
-
-# start_epoch=int(dt.datetime(2017, 1, 30).timestamp())
-
-# submissions = api.search_submissions(after=start_epoch,
-#                             subreddit='wallstreetbets',
-#                             filter=['url','author', 'title', 'subreddit'])
-
-# for submission in submissions:
-#     print(submission.title)
 from io import StringIO
 import praw
 import csv
@@ -25,34 +12,24 @@ reddit = praw.Reddit(
     password="uottahack2021"
 )
 
-subreddit = reddit.subreddit("stocks")
+subreddit = reddit.subreddit("pennystocks")
 
 with open("stocklist.txt") as f:
     stockList = f.readlines()
     stockList = [x.strip('\n') for x in stockList] 
-# you may also want to remove whitespace characters like `\n` at the end of each line
-#print(stockList)
 
 # assume you have a Subreddit instance bound to variable `subreddit`
 substring = "GME"
-for submission in subreddit.new():#limit=100):
+for submission in subreddit.new(limit=500):
     currPost = submission.title.encode('cp1252', errors='replace').decode('cp1252') 
-    #print(currPost)
     for stock in stockList:
-        if str(stock) in currPost:
+        if (" " + str(stock) + " ") in currPost:
+            print('\n******')
             print("STOCK: " + stock)
             print("TITLE: " + currPost)
-    #if any(stock in currPost for stock in stockList):
-        # print(stock)
-        # print("TITLE: " + currPost)
-    #if any(str(stock) in li for stock in stock_list):
-    #print("Found: " + stock)
-       # print(li)
-    #print("POST TITLE: " + submission.title.encode('cp1252', errors='replace').decode('cp1252'))  # Output: the submission's title
-    # print(submission.url)    # Output: the URL the submission points to
-    #print()   
-    # for top_level_comment in submission.comments:
-    #     if isinstance(top_level_comment, MoreComments):
-    #         continue
-    #     print(top_level_comment.body.encode('cp1252', errors='replace').decode('cp1252'))
-    # #print()
+            print('******\n')
+            submission.comments.replace_more(limit=None)
+            for top_level_comment in submission.comments:
+                for second_level_comment in top_level_comment.replies:
+                    print(top_level_comment.body.encode('cp1252', errors='replace').decode('cp1252'))
+
